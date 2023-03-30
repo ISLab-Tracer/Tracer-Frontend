170b6a940b89e0ac818e37c81c8cda5c6fec7eef
Ha-no <gksdn1216@naver.com>
2023-03-30 22:04:23 +0900

    [UPDATE] Excel Parsing
    *머지하면 안됨*
    Excel Parsing
    - 오류 수정 해야 함

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -117,3 +117,20 @@ export const writeBuffer = (str, format = 'base64') => {
 export const readBuffer = (str, format = 'base64') => {
   return Buffer.from(str, format).toString('utf8');
 };
+
+export const ExcelParsing = ({ setField, setRow, target } = props) => {
+  const file = target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = (e) => {
+    const data = target.result;
+    const workbook = XLSX.read(data, { type: 'binary' });
+
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    const rowData = XLSX.utils.sheet_to_json(worksheet, { header: 0 });
+    const fieldHeader = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+    setField(fieldHeader);
+    setRow(rowData);
+  };
+};
