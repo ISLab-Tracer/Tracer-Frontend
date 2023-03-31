81e25ee61fd9f9a8ec07f4d82369b57e6024a213
simonieee <75366051+simonieee@users.noreply.github.com>
2023-03-31 10:37:03 +0900

    [UPDATE] 바코드 qr 테스트
    - 바코드 qr 테스트

--- src/Routes/Pages/Barcode/BarcodePresenter.jsx
+++ src/Routes/Pages/Barcode/BarcodePresenter.jsx
@@ -1,25 +1,39 @@
 import { Button } from '@mui/material';
 import { EquipInfo } from 'Components';
-import React from 'react';
+import React, { useState } from 'react';
 import Barcode from 'react-barcode';
+import { QRCodeCanvas } from 'qrcode.react';
 
 const BarcodePresenter = () => {
   /* Router */
   /* State */
-  //   const [barcode, setBarcode] = useState();
+  const [barcode, setBarcode] = useState();
   /* Hooks */
   /* Functions */
+  const handleCreateBarcode = () => {
+    let num = '';
+    for (let i = 0; i < 13; i++) {
+      num += Math.floor(Math.random() * 10);
+    }
+    setBarcode(num);
+  };
   /* Render */
   return (
     <div className="main-content-container">
       <div style={{ display: 'flex', flexWrap: 'nowrap' }}>
-        <EquipInfo.Input label="바코드" />
-        <Button variant="outlined" size="small" sx={{ width: 100, height: 40 }}>
+        <EquipInfo.Input label="바코드" value={barcode} />
+        <Button
+          variant="outlined"
+          size="small"
+          sx={{ width: 100, height: 40 }}
+          onClick={handleCreateBarcode}
+        >
           자동생성
         </Button>
       </div>
-      <Barcode value="1604b772-adc0-4212-8a90-81186c57f598" />
+      <Barcode value={barcode} />
       <hr style={{ marginBottom: 30 }} />
+      <QRCodeCanvas value={`http://10.0.4.200:3333/${barcode}`} />
     </div>
   );
 };
