f27ca73285974b381bbb9f2fca79daec6f85be51
ongdv <ongdv@pusan.ac.kr>
2023-04-03 14:30:39 +0900

    [UPDATE] Assignment Update
    - 데이터 그리드 사용시,id를 만들어줘야함

--- src/Routes/Pages/Assignment/AssignmentPresenter.jsx
+++ src/Routes/Pages/Assignment/AssignmentPresenter.jsx
@@ -1,33 +1,33 @@
 import { Box } from '@mui/material';
-// import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid } from '@mui/x-data-grid';
 import { PageHeader } from 'Components';
 import { useCommonData } from 'Hooks/CommonDataManager';
 import React from 'react';
 
-// const columns = [
-//   { field: 'project_id', headerName: 'ID', width: 100 },
-//   {
-//     field: 'project_title',
-//     headerName: '과제명',
-//     width: 200,
-//     editable: true,
-//   },
-//   {
-//     field: 'project_desc',
-//     headerName: '설명',
-//     width: 300,
-//     editable: true,
-//   },
-// ];
+const columns = [
+  { field: 'project_id', headerName: 'ID', width: 100 },
+  {
+    field: 'project_title',
+    headerName: '과제명',
+    width: 200,
+    editable: true,
+  },
+  {
+    field: 'project_desc',
+    headerName: '설명',
+    width: 300,
+    editable: true,
+  },
+];
 
 const AssignmentPresenter = () => {
   /* Router */
   /* State */
-  const { categoryList, projectList, userList } = useCommonData();
-  // const [data, setData] = useState(projectList);
-  console.log(categoryList);
-  console.log(projectList);
-  console.log(userList);
+  const { projectList } = useCommonData();
+  const dataList = projectList.map((item) => {
+    const { project_id } = item;
+    return { ...item, id: project_id };
+  });
 
   /* Hooks */
   /* Functions */
@@ -36,8 +36,8 @@ const AssignmentPresenter = () => {
     <div className="main-content-container">
       <PageHeader title="과제" subTitle="데이터 관리" />
       <Box sx={{ height: 800, width: '100%' }}>
-        {/* <DataGrid
-          rows={data}
+        <DataGrid
+          rows={dataList}
           columns={columns}
           initialState={{
             pagination: {
@@ -50,7 +50,7 @@ const AssignmentPresenter = () => {
           checkboxSelection
           disableRowSelectionOnClick
           // onRowSelectionModelChange={(id) => handleSelect(id)}
-        /> */}
+        />
       </Box>
     </div>
   );
